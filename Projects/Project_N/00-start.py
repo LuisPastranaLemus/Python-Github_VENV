208fa5ca13daadffd9d1b83bda945ff146aaf9aa
LuisPastranaLemus <167579946+LuisPastranaLemus@users.noreply.github.com>
2025-05-04 13:51:41 -0600

    202505041351

---
+++ Projects/Project_N/00-start.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+pd.set_option('display.max_rows', 25) # show maximum 25 rows
+pd.set_option('display.max_columns', 25) # show maximum 10 columns
+pd.set_option('display.max_colwidth', 15) # show maximum 50 characters in each column
+pd.set_option('display.width', 150) # Mostrar 150 caracteres como máximo
+
+# from IPython.paths import get_ipython_dir
+# print(os.path.join(get_ipython_dir(), 'profile_default', 'startup'))
+# Make this file xists in the path output from the last code line
